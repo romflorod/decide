39eff376898723d78dfbc1f1bdcc407c6e5e4a0b
Beatriz Llamas <beallasai@alum.us.es>
2021-12-09 10:58:26 +0100

    Añadido método D´Hont

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -19,6 +19,7 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+
     def HuntingtonHill(self,numEscanos,options):
     
         votosTotales = 0
@@ -85,9 +86,31 @@ class PostProcView(APIView):
         return Response(options)
 
 
+    def dHont(self, options, numEscanos):
+
+        #Añadimos un campo para el contador de escaños asignados a cada opción
+        for op in options:
+            op['postproc'] = 0
+        
+        #Para cada escaño recorremos todas las opciones usando la fórmula de d'Hont: número de votos de esa opción/(número de escaños asignados a esa opción + 1)
+        for escano in range(0, numEscanos):
+             #Lista de tamaño igual al número de opciones. Recuento al aplicar la fórmula a cada opción (ordenados en la misma forma)
+            recuento = []
+            for op in options:
+                r = op['votes'] / (op['postproc']+1)
+                recuento.append(r)
+            
+            #Obtenemos el índice del máximo valor en la lista de recuento de votos (del ganador del escaño)
+            ganador = recuento.index(max(recuento))
+            #En la posicion del ganador le sumamos 1 escaño
+            options[ganador]['postproc'] += 1
+
+        return Response(options)
+
+
     def post(self, request):
         """
-         * type: IDENTITY | EQUALITY | WEIGHT
+         * type: IDENTITY | HUNTINGTONHILL | DHONT
          * options: [
             {
              option: str,
@@ -100,8 +123,15 @@ class PostProcView(APIView):
 
         t = request.data.get('type', 'IDENTITY')
         opts = request.data.get('options', [])
+        numEscanos = request.data.get('numEscanos', 0)
 
         if t == 'IDENTITY':
             return self.identity(opts)
+
+        elif t == 'HUNTINGTONHILL':
+            return self.HuntingtonHill(options=opts, numEscanos=numEscanos)
+
+        elif t == 'DHONT':
+            return self.dHont(options=opts, numEscanos=numEscanos)
 
         return Response({})
