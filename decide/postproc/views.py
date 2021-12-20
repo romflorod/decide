b234519060cc00e980c45c43d3901bc96e46917c
Aureliano Piqueras Rodríguez <aurpiqrod@alum.us.es>
2021-12-20 15:16:09 +0100

    feat(bipartisanship): Create function
    Feature(Bipartisanship): Creación de la función bipartisanship y cambios en el post

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -1,5 +1,8 @@
+from django.db import models
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import math
+import numpy as np
 
 
 class PostProcView(APIView):
@@ -16,9 +19,33 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+
+    def bipartitanship(self, options, numEscanyos):
+        def sortByVotes(e):
+            return e['votes']
+        opts = options
+        opts.sort(reverse=True,key=sortByVotes)
+        option1=opts[0]
+        option2=opts[1]
+        votosTotales = option1['votes'] + option2['votes']
+        proporcion = numEscanyos/votosTotales
+        escanyos1 = round(option1['votes']*proporcion)
+        escanyos2 = round(option1['votes']*proporcion)
+        sumaEscanyos = escanyos1 + escanyos2
+        if sumaEscanyos != numEscanyos:
+            sobrante = numEscanyos-sumaEscanyos
+            escanyos1 +=sobrante
+        for op in opts:
+            op['postproc'] = 0
+        opts[0]['postproc'] = escanyos1
+        opts[1]['postproc'] = escanyos2
+        options = opts
+        return Response(options)
+
+
     def post(self, request):
         """
-         * type: IDENTITY | EQUALITY | WEIGHT
+         * type: IDENTITY | BIPARTITANSHIP
          * options: [
             {
              option: str,
@@ -31,8 +58,12 @@ class PostProcView(APIView):
 
         t = request.data.get('type', 'IDENTITY')
         opts = request.data.get('options', [])
+        numEscanyos = request.data.get('numEscanyos', 0)
 
         if t == 'IDENTITY':
             return self.identity(opts)
+
+        elif t == 'BIPARTISHANSHIP':
+            return self.bipartishanship(options=opts, numEscanyos=numEscanyos)
 
         return Response({})
