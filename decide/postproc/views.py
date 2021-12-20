6d8a53052203c28f10db786a223d12cee0ac535e
romflorod <romanflodriguez13@gmail.com>
2021-12-20 18:41:53 +0100

    Finalización HuntingtonHill y testeado

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -19,35 +19,34 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
-    def HuntingtonHill(self,numEscanos,options):
+    def HuntingtonHill(self,options,numEscanyos):
     
         votosTotales = 0
         for x in options:
             votosTotales += x['votes']
         
-        if votosTotales > 0 and numEscanos > 0:
+        if votosTotales > 0 and numEscanyos > 0:
 
-            limite = votosTotales/numEscanos
+            limit = votosTotales/numEscanyos
 
-            #Crear parametros para metodo rounding rule
-            rounding = limite*0.001
-            lower = limite-rounding
-            upper = limite+rounding
+            #Vamos a aplicar la regla rounding
+            rounding = limit*0.001
+            lower = limit-rounding
+            upper = limit+rounding
 
-            numEscanosAsig = 0
+            numEscanyosAsig = 0
 
-            while(numEscanosAsig != numEscanos):
+            while(numEscanyosAsig != numEscanyos):
 
-                #si llegamos a aplicar rounding rule y no llegamos al numero igual de escanos, 
-                #reseteamos de nuevo el numero de escanos asig y empezamos de nuevo
-                numEscanosAsig = 0
+              ##si no se cumple la regla reseteamos  cero
+                numEscanyosAsig = 0
 
                 for x in options:
 
-                    if(x['votes']<limite):
+                    if(x['votes']<limit):
                         x['postproc']=0
                     else:
-                        cuota = x['votes']/limite
+                        cuota = x['votes']/limit
                         
                         if(isinstance(cuota,int)):
                             x['postproc']=cuota
@@ -61,22 +60,24 @@ class PostProcView(APIView):
                             else:
                                 x['postproc']=lQ
                 
-                    numEscanosAsig += x['postproc']
+                    numEscanyosAsig += x['postproc']
 
-                #Huntington-Hill Rounding Rule
-                #For a quota q, let L denote its lower quota, U its upper quota, and G the
+                #Rounding Rule:
+                
+                
+                #For a quota q, let L denote it's lower quota, U its upper quota, and G the
                 #geometric mean of L and U. If then round q down to L, otherwise
                 #round q up to U.
 
-                if(numEscanosAsig < numEscanos):
-                    limite = lower
-                    lower = limite-rounding
-                    upper = limite+rounding
+                if(numEscanyosAsig < numEscanyos):
+                    limit = lower
+                    lower = limit-rounding
+                    upper = limit+rounding
 
                 else:
-                    limite = upper
-                    lower = limite-rounding
-                    upper = limite+rounding
+                    limit = upper
+                    lower = limit-rounding
+                    upper = limit+rounding
         else:
             for x in options:
                 x.update({'postproc' : 0})
@@ -100,8 +101,13 @@ class PostProcView(APIView):
 
         t = request.data.get('type', 'IDENTITY')
         opts = request.data.get('options', [])
+        numEscanyos = request.data.get('numEscanyos', 0)
+
 
         if t == 'IDENTITY':
             return self.identity(opts)
+        elif t=='HUNTINGTONHILL':
+            return self.HuntingtonHill(options=opts, numEscanyos=numEscanyos)
+            
 
         return Response({})
