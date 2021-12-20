cc42eee9fe8d95ace0e3610355b4e750c84c43fc
Aureliano Piqueras Rodríguez <aurpiqrod@alum.us.es>
2021-12-20 15:47:29 +0100

    feat(bipartisanship): comments
    añadidos comentarios a la función bipartisanship

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -21,24 +21,30 @@ class PostProcView(APIView):
 
 
     def bipartitanship(self, options, numEscanyos):
+        # función de ordenación
         def sortByVotes(e):
             return e['votes']
+        # copiamos las opciones, ordenamos y obtenemos las dos con mayor número de votos        
         opts = options
         opts.sort(reverse=True,key=sortByVotes)
         option1=opts[0]
         option2=opts[1]
+        #calculamos la proporción de peso por cada voto   
         votosTotales = option1['votes'] + option2['votes']
         proporcion = numEscanyos/votosTotales
         escanyos1 = round(option1['votes']*proporcion)
         escanyos2 = round(option2['votes']*proporcion)
         sumaEscanyos = escanyos1 + escanyos2
+        #si queda algún escaño sin asignar se lo damos al primero
         if sumaEscanyos != numEscanyos:
             sobrante = numEscanyos-sumaEscanyos
             escanyos1 +=sobrante
+        #inicializamos y asignamos los escaños a los dos primeros
         for op in opts:
             op['postproc'] = 0
         opts[0]['postproc'] = escanyos1
         opts[1]['postproc'] = escanyos2
+        #reemplazamos las opciones y devolvemos el resultado
         options = opts
         return Response(options)
 
